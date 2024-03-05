ee2d628862f32382bb2833dd1468dbf3c5ee8777
MaterialFuture <konstantinev@protonmail.com>
2024-03-05 12:36:15 -0600

    init

---
+++ README.md
@@ -0,0 +1,31 @@
+## Groq-Telegram
+
+A template for chatting with Groq models in Telegram.
+
+## Prerequisites
+
+- Groq API key via the [developer panel](https://console.groq.com/keys).
+- Create a bot with [BotFather](https://t.me/botfather).
+
+## Roadmap
+TBD
+
+## Prerequisites
+
+- I prefer [bun]() to npm, but the commands are the same for npm/yarn/pnpm etc. But make sure that's installed.
+
+
+## Dev / Testing
+
+_Note: Make sure to setup your bot via BotFather and start a chat with it_
+
+Start the bot script:
+`bun run dev`
+
+Start typing to your bot.
+
+I/O should be printed in the console so you can see.
+
+## Deploy
+
+`bun run start`
---
+++ src/bot.js
@@ -0,0 +1,39 @@
+import { Bot } from "grammy";
+import Groq from "groq-sdk";
+
+const groq = new Groq({
+  apiKey: process.env.GROQ_API_KEY,
+});
+
+const bot = new Bot(
+  process.env.TELEGRAM_BOT_TOKEN,
+);
+
+async function getGroqResponse(query) {
+  try {
+    const completion = await groq.chat.completions.create({
+      messages: [
+        {
+          role: "user",
+          content: query,
+        },
+      ],
+      model: "mixtral-8x7b-32768",
+    });
+
+    return completion.choices[0].message.content;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+bot.on("message:text", async (ctx) => {
+  const response = await getGroqResponse(ctx.message.text);
+
+  console.log('Message from: ', ctx.from.username)
+  console.log('Message: ', ctx.message.text)
+
+  ctx.reply(response);
+});
+
+bot.start();
